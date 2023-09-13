9874ec0a2d76f5ba21a42d9c5dca449191f3f566
biarwf <142931491+biarwf@users.noreply.github.com>
2023-09-13 08:25:46 -0300

    Exercício Média

---
+++ exercicio média.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <math.h>
+
+main(){
+	float n1,n2,n3, med =0;
+	printf("\nDigite a n1 -> ");
+	scanf("%f",&n1);
+	printf("\nDigite a n2 -> ");
+	scanf("%f",&n2);
+	printf("\nDigite a n3 -> ");
+	scanf("%f",&n3);
+	med = (n1+n2+n3)/3;
+	printf("\nA média das notas do aluno é %0.2f ->" ,med);
+	
+	
+}
